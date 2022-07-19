c61fa9b170e0c61ef4f10acd818364b164ec9721
TacoJimmy <t5669016@ntut.org.tw>
2022-07-20 06:18:48 +0800

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -5,13 +5,16 @@ from modbus_tk import modbus_rtu
 
 def evi_th():
     evi_th_value = [0,0]
-    master = modbus_rtu.RtuMaster(serial.Serial(port='/dev/ttyS1', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
-    master.set_timeout(5.0)
-    master.set_verbose(True)
-    evi_th_data = master.execute(1, cst.READ_INPUT_REGISTERS, 1, 2)
-    evi_th_value[0] = evi_th_data[0]*0.1
-    evi_th_value[1] = evi_th_data[1]*0.1
-    time.sleep(0.5)
+    try:
+        master = modbus_rtu.RtuMaster(serial.Serial(port='/dev/ttyS1', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        evi_th_data = master.execute(1, cst.READ_INPUT_REGISTERS, 1, 2)
+        evi_th_value[0] = evi_th_data[0]*0.1
+        evi_th_value[1] = evi_th_data[1]*0.1
+        time.sleep(0.5)
+    except:
+        evi_th_value = [0,0]
 
     return (evi_th_value)
 
