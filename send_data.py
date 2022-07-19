7612f66e5ee7328acdf69a1afe8d9506ac7b0e9d
TacoJimmy <t5669016@ntut.org.tw>
2022-07-20 05:50:56 +0800

    Update send_data.py

--- send_data.py
+++ send_data.py
@@ -1,10 +1,18 @@
 import requests
 import time
+import get_data
 
 def req_post():
-    payload = {'data':60}
-    html = requests.post('https://strawberry.cgptiot.com.tw/iot/Humidity.php?id=1680001-1',data = payload)
-    print(html.text) 
+    try:
+        cgpt_th = get_data.evi_th()
+        temp_payload = {'data':cgpt_th[0]}
+        html = requests.post('https://strawberry.cgptiot.com.tw/iot/Temperature.php?id=1680001-1',data = temp_payload)
+        #humi_payload = {'data':cgpt_th[1]}
+        #html = requests.post('https://strawberry.cgptiot.com.tw/iot/Humidity.php?id=1680001-1',data = humi_payload)
+        
+        print(html.text) 
+    except:
+        print('error')
 
 if __name__ == '__main__':
     while True:
