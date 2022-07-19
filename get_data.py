5095ed55ffff511ed2f9940ba3c9f853ff3b8319
TacoJimmy <t5669016@ntut.org.tw>
2022-07-19 18:34:57 +0800

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -7,7 +7,7 @@ def evi_th():
     master = modbus_rtu.RtuMaster(serial.Serial(port='/dev/ttyS1', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
     master.set_timeout(5.0)
     master.set_verbose(True)
-    evi_th_data = master.execute(1, cst.READ_HOLDING_REGISTERS, 1, 2)
+    evi_th_data = master.execute(1, cst.READ_INPUT_REGISTERS, 1, 2)
     time.sleep(0.5)
 
     return evi_th_data
