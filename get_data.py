a342b91b9643d5ece91ffb1ee5e9049e3e49d863
TacoJimmy <t5669016@ntut.org.tw>
2022-07-25 19:41:31 +0800

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -5,27 +5,27 @@ from modbus_tk import modbus_rtu
 
 def evi_th():
     evi_th_value = [0,0,0,0,0]
-    try:
-        master = modbus_rtu.RtuMaster(serial.Serial(port='/dev/ttyS1', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
-        master.set_timeout(5.0)
-        master.set_verbose(True)
-        evi_th_data = master.execute(1, cst.READ_INPUT_REGISTERS, 1, 2) #0溫度 1濕度
-        time.sleep(2)
-        evi_wc_data01 = master.execute(2, cst.READ_HOLDING_REGISTERS, 1, 3) #1無人 # 2臭味 3乳液
-        time.sleep(2)
-        evi_wc_data02 = master.execute(3, cst.READ_HOLDING_REGISTERS, 2, 2) # 2廁所紙 3人流
-        time.sleep(2)
+    #try:
+    master = modbus_rtu.RtuMaster(serial.Serial(port='/dev/ttyS1', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+    master.set_timeout(5.0)
+    master.set_verbose(True)
+    evi_th_data = master.execute(1, cst.READ_INPUT_REGISTERS, 1, 2) #0溫度 1濕度
+    time.sleep(2)
+    evi_wc_data01 = master.execute(2, cst.READ_HOLDING_REGISTERS, 1, 3) #1無人 # 2臭味 3乳液
+    time.sleep(2)
+    evi_wc_data02 = master.execute(3, cst.READ_HOLDING_REGISTERS, 2, 2) # 2廁所紙 3人流
+    time.sleep(2)
         
-        evi_th_value[0] = evi_th_data[0]*0.1 #溫度
-        evi_th_value[1] = evi_th_data[1]*0.1 #濕度
-        evi_th_value[2] = evi_wc_data01[2]   #乳液
-        evi_th_value[3] = evi_wc_data02[0]   #廁所紙
-        evi_th_value[4] = evi_wc_data02[1]   #人流
-        evi_th_value[5] = evi_wc_data01[0]   #無人
-        evi_th_value[6] = evi_wc_data01[1]   #臭味
-        time.sleep(0.5)
-    except:
-        evi_th_value = [0,0,0,0,0,0,0]
+    evi_th_value[0] = evi_th_data[0]*0.1 #溫度
+    evi_th_value[1] = evi_th_data[1]*0.1 #濕度
+    evi_th_value[2] = evi_wc_data01[2]   #乳液
+    evi_th_value[3] = evi_wc_data02[0]   #廁所紙
+    evi_th_value[4] = evi_wc_data02[1]   #人流
+    evi_th_value[5] = evi_wc_data01[0]   #無人
+    evi_th_value[6] = evi_wc_data01[1]   #臭味
+    time.sleep(0.5)
+    #except:
+        #evi_th_value = [0,0,0,0,0,0,0]
 
     return (evi_th_value)
     #return (evi_wc_data01)
